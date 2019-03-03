a425bbddb2b26632d01523f54f1347b3ca20e0ad
Nathan Cox <nccox2@ncsu.edu>
2019-03-03 17:17:03 -0500

    Working conversion.  Output format not as expected.

--- src/ACARS/Create_ACARS_Map_CSV-1-2.py
+++ src/ACARS/Create_ACARS_Map_CSV-1-2.py
@@ -15,16 +15,16 @@ def get_ACARS_KMZ_file(filename='acarsMap.kmz'):
     return r
 
 def convert_KMZ_to_CSV(kmzfile):
-    result = keyhole.keyholemarkup2x(kmzfile,output='csv'):
+    result = keyhole.keyholemarkup2x(kmzfile,output='csv')
+    print("4")
+    print(result)
     return result
 
 def main():
-    """
-    Open the KML. Read the KML. Open a CSV file. Process a coordinate string to be a CSV row.
-    """
+    #kmz_filename = 'D:\\coxna\\Documents\\Flight Sim\\Dispatch\\dva_dispatch_tools\\data\\ACARS\\acarsMap.kmz';
     kmz_filename = 'acarsMap.kmz';
     kmz = get_ACARS_KMZ_file(filename=kmz_filename)
-    csv = convert_KMZ_to_CSV
+    csv = convert_KMZ_to_CSV(kmz_filename)
     
 if __name__ == "__main__":
     main()
